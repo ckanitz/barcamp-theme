be83609ddb6dd1cf279f830a19be4add2cf57ac4
Dennis Erdmann <denniserdmann@dennismb.local>
2013-03-09 15:34:53 +0100

    added template-files

---
+++ functions.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Twenty Twelve functions and definitions.
+ *
+ * Sets up the theme and provides some helper functions, which are used
+ * in the theme as custom template tags. Others are attached to action and
+ * filter hooks in WordPress to change core functionality.
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development and
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
+ * (those wrapped in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before the parent
+ * theme's file, so the child theme functions would be used.
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
+ * to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
+ *
+ * @package WordPress
+ * @subpackage Barcamp
+ * @since Twenty Twelve 1.0
+ */
+
+
+function barcamp_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Main Sidebar', 'twentytwelve' ),
+		'id' => 'sidebar-1',
+		'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'twentytwelve' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Startseiten Widget', 'twentytwelve' ),
+		'id' => 'sidebar-2',
+		'description' => __( '3 Textboxen unterhalb des Einleitungstextes', 'twentytwelve' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+	register_sidebar( array(
+		'name' => __( 'Social Plugins', 'twentytwelve' ),
+		'id' => 'sidebar-3',
+		'description' => __( 'Social Plugins Facebook Twitter Google Plus', 'twentytwelve' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+add_action( 'widgets_init', 'barcamp_widgets_init' );
---
+++ header.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php /* -- LESS CSS -- */
+  require "lib/lessphp/lessc.inc.php";
+  $less = new lessc;
+  //$less->checkedCompile(get_stylesheet_directory().'/style.less', get_stylesheet_directory().'/style-min.css');
+  $cache = $less->cachedCompile(get_stylesheet_directory().'/style.less');
+  file_put_contents(get_stylesheet_directory().'/style-min.css', $cache["compiled"]);
+/* -- END LESS CSS -- */ ?>
+<?php wp_head(); ?>
+<link rel="stylesheet" href="<?php echo bloginfo('stylesheet_directory')?>/style-min.css" type="text/css" media="all">
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<header id="masthead" class="site-header" role="banner">
+	<div class="logo">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<img src="<?php echo bloginfo('stylesheet_directory')?>/images/logo-bcki.png" alt="Barcamp Kiel Logo" width="250" height="70" />
+		</a>
+	</div>
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
+			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
+		
+		<?php if ( has_post_thumbnail() ) : ?>
+		<figure class="headerimage">
+			<?php the_post_thumbnail('full'); ?>
+			<figcaption>
+				<hgroup>
+					<h1 class="site-title"><?php echo get_post(get_post_thumbnail_id())->post_title; ?></h1>
+					<h2 class="site-description"><?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?></h2>
+				</hgroup>
+			</figcaption>
+		</figure>
+		
+		<?php else: ?>
+				<?php $header_image = get_header_image();
+				if ( ! empty( $header_image ) ) : ?>
+					<figure class="headerimage">
+						<img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" 
+						height="<?php echo get_custom_header()->height; ?>" alt="Sessionplanung beim Barcamp Kiel" />
+						<figcaption>
+							<hgroup class="site-info">
+							<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+							<h2 class="site-description"><?php echo str_replace('|','<br />',get_bloginfo('description')); ?></h2>
+							</hgroup>
+						</figcaption>
+					</figure>
+				<?php endif; ?>
+
+		<?php endif; ?>
+	</header><!-- #masthead -->
+
+	<div id="main" class="wrapper">
